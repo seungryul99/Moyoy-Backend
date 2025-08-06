3cf57a619760f379953e17ff2651728c3428c16a
KimSeungRyul <seunglul1999@gmail.com>
2025-08-06 18:00:58 +0900

    실행가능 모듈에 SpringBootApplication 추가

---
+++ Moyoy-Api/src/main/java/com/moyoy/api/MoyoyApiApplication.java
@@ -0,0 +1,11 @@
+package com.moyoy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyoyApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MoyoyApiApplication.class, args);
+	}
+}
---
+++ Moyoy-Batch/src/main/java/com/moyoy/batch/MoyoyBatchApplication.java
@@ -0,0 +1,11 @@
+package com.moyoy.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyoyBatchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MoyoyBatchApplication.class, args);
+	}
+}
